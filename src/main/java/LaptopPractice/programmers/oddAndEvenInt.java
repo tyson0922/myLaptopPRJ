37f76b747c1b0422510ce1b4612e589ad174c2d0
tyson0922 <taeseung92l@gmail.com>
2024-07-11 10:44:45 +0900

    source upload

---
+++ src/main/java/LaptopPractice/programmers/oddAndEvenInt.java
@@ -0,0 +1,14 @@
+package LaptopPractice.programmers;
+import java.util.Scanner;
+
+public class oddAndEvenInt {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n%2==0){
+            System.out.print(n + " is even");
+        } else {
+            System.out.print(n + " is odd");
+        }
+    }
+}
