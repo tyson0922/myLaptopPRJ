12bf249ebf015998ccdf9ed04557e9c6b671498f
tyson0922 <taeseung92l@gmail.com>
2024-07-12 10:46:48 +0900

    source upload

---
+++ src/main/java/LaptopPractice/programmers/overWriteString.java
@@ -0,0 +1,8 @@
+package LaptopPractice.programmers;
+class overWriteString {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = my_string.substring(0,s) + overwrite_string + my_string.substring(s + overwrite_string.length(), my_string.length());
+
+        return answer;
+    }
+}
