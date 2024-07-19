dbbf9fa0245dd8594255c1f289c9039a19fa277d
tyson0922 <taeseung92l@gmail.com>
2024-07-19 14:36:59 +0900

    source upload

---
+++ src/main/java/LaptopPractice/JavaBook/Code10_08.java
@@ -0,0 +1,11 @@
+package LaptopPractice.JavaBook;
+
+public class Code10_08 {
+    static void method1() {
+        System.out.println("반환값이 없는 메서드 실행");
+    }
+
+    public static void main(String[] args){
+        method1();
+    }
+}
