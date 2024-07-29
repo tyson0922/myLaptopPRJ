6f62cd9bcc216ff5646b44585305d7fed895b462
tyson0922 <taeseung92l@gmail.com>
2024-07-29 21:45:04 +0900

    commit

---
+++ src/main/java/LaptopPractice/JavaBook/Code11_03.java
@@ -0,0 +1,23 @@
+package LaptopPractice.JavaBook;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Code11_03 {
+    public static void main (String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new FileReader("c:/FirstJava/myData1.txt"));
+        String inStr;
+        while(true) {
+            inStr = br.readLine();
+            if(inStr == null)
+                break;
+            sb.append(inStr).append("\n");
+        }
+        br.close();
+        sb.reverse();
+        System.out.println(sb);
+    }
+}
