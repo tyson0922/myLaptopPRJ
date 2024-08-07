d6c5392f2644216959d78ed77fbf023f2e8ce91c
tyson0922 <taeseung92l@gmail.com>
2024-08-07 23:02:18 +0900

    commit

---
+++ src/main/java/LaptopPractice/JavaBook/Code12_01.java
@@ -0,0 +1,14 @@
+package LaptopPractice.JavaBook;
+
+import javax.swing.*;
+
+public class Code12_01 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("기본 윈도우 창");
+
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+    }
+}
